3eef4d326ca9bdc3c832aff6b84da899dbad4790
Rachel T <rachmaybe@gmail.com>
2019-08-23 23:06:56 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,6 +11,9 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-  
+  if age < 12 
+   puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+ elseif 
+   puts "You are too old for this."
 end 
   
